11baaecbe7e676514755365b506c8d731aa3fdcf
Abas <amirpc36@gmail.com>
2025-11-26 09:15:42 +0330

    Update problem_002.rs

--- Rust-Euler/src/bin/problem_002.rs
+++ Rust-Euler/src/bin/problem_002.rs
@@ -2,16 +2,16 @@ fn main() {
     let mut sum: i64 = 2;
     let mut current: i64 = 2;
     let mut previous: i64 = 1;
-    let mut middle: i64 = 0;
+    let mut temp: i64 = 0;
     //let mut step: i32 = 2;
     while current < 4_000_000 {
         //step += 1;
-        middle = current;
+        temp = current;
         current += previous;
-        previous = middle;
+        previous = temp;
         // println!(
-        //     "current is : {} and middle is {} on step : {}",
-        //     current, middle, step
+        //     "current is : {} and temp is {} on step : {}",
+        //     current, temp, step
         // );
         if current % 2 == 0 {
             sum += current;
