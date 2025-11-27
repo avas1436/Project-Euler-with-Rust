0c845b1b74ba337a374730bfc82edf8c03435fda
avas <amirpc36@gmail.com>
2025-11-27 17:04:45 +0330

    edith problem_003

---
+++ Rust-Euler/src/bin/problem_003.rs
@@ -0,0 +1,57 @@
+use std::io;
+
+fn main() {
+    let num: i64 = take_number();
+    for i in (((num as f64).sqrt() as i64)..num).rev() {
+        if num % i == 0 && is_prime(i) {
+            println!("The biggest prime factor of {} is {}", num, i);
+            break;
+        };
+    }
+}
+
+// let mut n: bool = false;
+
+// for i in (1..30).rev() {
+//     n = is_prime(i);
+//     if n {
+//         println!("number {} is prime", i);
+//     }
+// }
+
+fn is_prime(n: i64) -> bool {
+    if n <= 1 {
+        return false;
+    };
+    if n == 2 {
+        return true;
+    };
+    if n % 2 == 0 {
+        return false;
+    };
+
+    let mut i: i64 = 3;
+
+    while i * i < n {
+        if n % i == 0 {
+            return false;
+        };
+        i += 2;
+    }
+    true
+}
+
+fn take_number() -> i64 {
+    let number = loop {
+        println!("Input a number to calculate biggest prime factor");
+        let mut input: String = String::new();
+        io::stdin()
+            .read_line(&mut input)
+            .expect("Failed to read input");
+        match input.trim().parse::<i64>() {
+            Ok(num) if num > 0 => break num,
+            _ => println!("Please enter a valid positive number"),
+        };
+    };
+    number;
+}
