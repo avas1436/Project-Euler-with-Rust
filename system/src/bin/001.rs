c3cfbb54b5dc7b01cd3f5f3db6a50bcfecb96569
abAsz <abas.zade@outlook.com>
2026-01-05 14:55:15 +0330

    initialize LRA Cache problem but its too hard to solve in this level

---
+++ system/src/bin/001.rs
@@ -0,0 +1,44 @@
+// Q1. LRU Cache
+//
+// Design a data structure that follows the constraints of a Least Recently Used (LRU) cache.
+
+// Implement the LRUCache class:
+
+// LRUCache(int capacity) Initialize the LRU cache with positive size capacity.
+// int get(int key) Return the value of the key if the key exists, otherwise return -1.
+// void put(int key, int value) Update the value of the key if the key exists. Otherwise,
+// add the key-value pair to the cache. If the number of keys exceeds the capacity from this
+// operation, evict the least recently used key.
+// The functions get and put must each run in O(1) average time complexity.
+
+
+struct LRUCache {
+
+}
+
+
+/**
+ * `&self` means the method takes an immutable reference.
+ * If you need a mutable reference, change it to `&mut self` instead.
+ */
+impl LRUCache {
+
+    fn new(capacity: i32) -> Self {
+
+    }
+
+    fn get(&self, key: i32) -> i32 {
+
+    }
+
+    fn put(&self, key: i32, value: i32) {
+
+    }
+}
+
+/**
+ * Your LRUCache object will be instantiated and called as such:
+ * let obj = LRUCache::new(capacity);
+ * let ret_1: i32 = obj.get(key);
+ * obj.put(key, value);
+ */
