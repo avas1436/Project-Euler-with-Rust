4fd55ba978eca3c37b55b92f84d970b259bd7c5b
abAsz <abas.zade@outlook.com>
2026-01-05 12:40:35 +0330

    its not work for solving this problem any more

--- datastructure/src/bin/002.rs
+++ datastructure/src/bin/002.rs
@@ -11,16 +11,22 @@ struct Solution;
 
 impl Solution {
     pub fn can_make_arithmetic_progression(arr: Vec<i32>) -> bool {
-        let index = arr.len();
-        let slice = &mut arr[0..index];
-        slice.sort();
-        let difference: i32 = slice[1] - slice[0];
-
-        for i in 2..index {
-            if slice[i] - slice[i - 1] != difference {
+        let length = arr.len() as i32;
+        if length < 2 {
+            return true;
+        };
+        let max = arr.iter().max().unwrap().clone();
+        let min = arr.iter().min().unwrap().clone();
+        let diff = (max - min) / (length - 1);
+        if diff == 0 {
+            return true;
+        };
+        for num in arr {
+            if (num - min) / diff != 0 {
                 return false;
             }
         }
+
         return true;
     }
 }
@@ -28,7 +34,9 @@ impl Solution {
 fn main() {
     let num1: Vec<i32> = vec![3, 5, 1];
     let num2: Vec<i32> = vec![1, 2, 4];
-    let num3: Vec<i32> = vec![1];
+    let num3: Vec<i32> = vec![
+        13, 12, -12, 9, 9, 16, 7, -10, -20, 0, 18, -1, -20, -10, -8, 15, 15, 16, 2, 15,
+    ];
     for num in [num1, num2, num3] {
         let result = Solution::can_make_arithmetic_progression(num);
         println!("Result: {}", result);
