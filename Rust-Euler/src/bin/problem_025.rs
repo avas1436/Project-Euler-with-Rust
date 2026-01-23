a6e8517d3d3c0890ae405ca7d72095559878ead1
abAsz <abas.zade@outlook.com>
2026-01-23 11:17:44 +0330

    feat(problem_025): pass all test of sum tow nums with infinitive digits.

--- Rust-Euler/src/bin/problem_025.rs
+++ Rust-Euler/src/bin/problem_025.rs
@@ -3,7 +3,7 @@
 
 fn main() {
     // let ans: u128 = answare(1000);
-    println! {"the answare is"};
+    println! {"the answare is : "};
 }
 
 // fn fibonacci_sequence_contain_digits(digits: usize) -> usize {
@@ -15,19 +15,34 @@ fn main() {
 fn sum_strings(a: &str, b: &str) -> String {
     let mut carry = 0;
     let mut result: String = String::new();
+    let mut iter_a = a.chars().rev();
+    let mut iter_b = b.chars().rev();
 
-    for i in (0..b.len()).rev() {
-        let da: usize = a[i].to_digit(10);
-        let db: usize = b[i].to_digit(10);
-        let c = da + db;
-        if c > 9 {
-            carry = c - 10;
-            result.push(carry.);
-        } else {
-            result.push(c);
+    let limit = b.len();
+    let mut step = 0;
+
+    loop {
+        let da = match iter_a.next() {
+            Some(c) => c.to_digit(10).unwrap(),
+            None => 0,
+        };
+        let db = iter_b.next().map_or(0, |c| c.to_digit(10).unwrap());
+
+        if step >= limit && db == 0 && da == 0 && carry == 0 {
+            break;
         }
+
+        let sum = da + db + carry;
+        result.push(char::from_digit(sum % 10, 10).unwrap());
+        carry = sum / 10;
+
+        step += 1;
     }
-    result.join("")
+    if carry != 0 {
+        result.push(char::from_digit(carry, 10).unwrap());
+    }
+
+    result.chars().rev().collect()
 }
 
 // test
@@ -39,24 +54,35 @@ mod tests {
     #[test]
     fn sum_1() {
         let result = sum_strings("1", "1");
+        println!("Result of 1 + 1 is : {}", result);
         assert_eq!(result, "2");
     }
 
     #[test]
     fn sum_8_5() {
         let result = sum_strings("5", "8");
+        println!("Result of 5 + 8 is : {}", result);
         assert_eq!(result, "13");
     }
 
     #[test]
     fn sum_2_digit() {
         let result = sum_strings("55", "89");
+        println!("Result of 55 + 89 is : {}", result);
         assert_eq!(result, "144");
     }
 
     #[test]
     fn sum_dif_nums() {
         let result = sum_strings("9", "98");
+        println!("Result of 9 + 98 is : {}", result);
         assert_eq!(result, "107");
+    }
+
+    #[test]
+    fn zero_test() {
+        let result = sum_strings("0", "0");
+        println!("Result of 0 + 0 is : {}", result);
+        assert_eq!(result, "0");
     }
 }
