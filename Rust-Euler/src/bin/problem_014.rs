6129089cd6b9af0d6908c8d06f5eb4483aa37370
abAsz <abas.zade@outlook.com>
2026-01-24 23:15:10 +0330

    fix(014): use usize instead of u128 and merge odd step and its next step to one step

--- Rust-Euler/src/bin/problem_014.rs
+++ Rust-Euler/src/bin/problem_014.rs
@@ -37,10 +37,11 @@ impl ColatzCalculator {
             }
             if num % 2 == 0 {
                 num /= 2;
+                step += 1;
             } else {
-                num = (3 * num) + 1;
+                num = ((3 * num) + 1) / 2;
+                step += 2;
             }
-            step += 1;
         }
         self.cache.insert(number, step);
         step
