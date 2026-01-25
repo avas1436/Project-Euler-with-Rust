37b46b851f0df95340dda4ff5bef8141c931e261
abAsz <abas.zade@outlook.com>
2026-01-25 18:04:08 +0330

    feat(014): intilize project structure.

--- Rust-Euler/src/bin/problem_021.rs
+++ Rust-Euler/src/bin/problem_021.rs
@@ -13,3 +13,32 @@
         220 -> 284
         284 -> 220
 */
+
+use std::collections::HashMap;
+
+fn main() {
+    todo!("solve problem here");
+}
+
+struct AmicableNumbers {
+    cache: HashMap<usize, usize>,
+}
+
+impl AmicableNumbers {
+    fn new() -> Self {
+        Self {
+            cache: HashMap::new(),
+        }
+    }
+    fn sum_of_proper_divisors(number: usize) {
+        todo!("this function should calculate sum of divisors of a number");
+    }
+
+    fn is_amicable(am: usize) -> bool {
+        todo!("this function determine amicable numbers");
+    }
+
+    fn sum_amicables(limit: usize) -> usize {
+        todo!("this function iterate from 1 to limit and sum all amicable numbers");
+    }
+}
