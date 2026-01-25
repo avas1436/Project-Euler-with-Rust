d75ac9bc2e8e59a9bdf27f028678b09f565e8a91
abAsz <abas.zade@outlook.com>
2026-01-25 17:46:30 +0330

    chore(021): start solving problem 21.

---
+++ Rust-Euler/src/bin/problem_021.rs
@@ -0,0 +1,15 @@
+/*
+    Project Euler - Problem 21
+    Amicable Numbers
+
+    This program finds the sum of all amicable numbers under a given limit.
+
+    Two numbers a and b are considered amicable if:
+    - The sum of proper divisors of a equals b
+    - The sum of proper divisors of b equals a
+    - a != b
+
+    For example:
+        220 -> 284
+        284 -> 220
+*/
