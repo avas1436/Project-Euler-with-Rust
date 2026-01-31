97026d542ee486cb8f87bb1570b26dd93d871d89
avAs zade <abas.zade@outlook.com>
2026-01-31 20:45:15 +0330

    feat(Queue-3): implemented structure and tests.

--- datastructure/src/bin/007.rs
+++ datastructure/src/bin/007.rs
@@ -18,3 +18,89 @@ Notes:
     simulate a stack using a list or deque (double-ended queue) as long as you use only
     a stack's standard operations.
 */
+
+fn main() {
+    todo!("answare run here!");
+}
+
+struct MyQueue {}
+
+/**
+ * `&self` means the method takes an immutable reference.
+ * If you need a mutable reference, change it to `&mut self` instead.
+ */
+impl MyQueue {
+    fn new() -> Self {}
+
+    fn push(&self, x: i32) {}
+
+    fn pop(&self) -> i32 {}
+
+    fn peek(&self) -> i32 {}
+
+    fn empty(&self) -> bool {}
+}
+
+/**
+ * Your MyQueue object will be instantiated and called as such:
+ * let obj = MyQueue::new();
+ * obj.push(x);
+ * let ret_2: i32 = obj.pop();
+ * let ret_3: i32 = obj.peek();
+ * let ret_4: bool = obj.empty();
+ */
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    struct MyQueueWorld {
+        command: Vec<String>,
+        input: Vec<Vec<i32>>,
+    }
+
+    impl MyQueueWorld {
+        fn new(command: Vec<String>, input: Vec<Vec<i32>>) -> Self {
+            Self { command, input }
+        }
+
+        fn run(&self) -> Vec<Option<i32>> {
+            let mut ans: Option<MyQueue> = None;
+            let mut result: Vec<Option<i32>> = Vec::new();
+
+            for (cmd, inp) in self.command.iter().zip(self.input.iter()) {
+                match cmd.as_str() {
+                    "MyQueue" => {
+                        ans = Some(MyQueue::new());
+                        result.push(None);
+                    }
+                    "push" => {
+                        ans.as_mut().unwrap().push(inp[0]);
+                        result.push(None);
+                    }
+                    "peek" => result.push(Some(ans.peek())),
+                    "pop" => result.push(Some(ans.pop())),
+                    "empty" => result.push(Some(ans.empty() as i32)),
+                }
+            }
+            result
+        }
+    }
+
+    #[test]
+    fn leet_code_test() {
+        let mut result = MyQueueWorld::new(
+            vec![
+                "MyQueue".to_string(),
+                "push".to_string(),
+                "push".to_string(),
+                "peek".to_string(),
+                "pop".to_string(),
+                "empty".to_string(),
+            ],
+            vec![vec![], vec![1], vec![2], vec![], vec![], vec![]],
+        );
+        result.run();
+        assert_eq!(result, vec![None, None, None, Some(1), Some(1), None]);
+    }
+}
