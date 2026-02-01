8bdcbe637fe03e82edf23bbf7a191f064ce26a18
avAs zade <abas.zade@outlook.com>
2026-02-01 17:27:55 +0330

    feat(Heap-1): implemented strucure and tests.

--- datastructure/src/bin/008.rs
+++ datastructure/src/bin/008.rs
@@ -15,3 +15,29 @@ At the end of the game, there is at most one stone left.
 
 Return the weight of the last remaining stone. If there are no stones left, return 0.
 */
+
+fn main() {
+    todo!("solver run here!");
+}
+
+struct Solution;
+
+impl Solution {
+    pub fn last_stone_weight(stones: Vec<i32>) -> i32 {}
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn last_stone_weight() {
+        assert_eq!(Solution::last_stone_weight(vec![2, 7, 4, 1, 8, 1]), 1);
+    }
+
+    #[test]
+    fn just_one_stone() {
+        assert_eq!(Solution::last_stone_weight(vec![2, 7, 4, 1, 8, 1]), 1);
+        assert_eq!(Solution::last_stone_weight(vec![1]), 1);
+    }
+}
