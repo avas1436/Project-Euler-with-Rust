bc0b4e126f4b0443e66034aabfef41a793ebacfa
avAs zade <abas.zade@outlook.com>
2026-02-03 20:16:23 +0330

    fix(heap-009): restart solving the problem.

--- datastructure/src/bin/009.rs
+++ datastructure/src/bin/009.rs
@@ -1,4 +1,4 @@
-use std::collections::BinaryHeap;
+use std::{cmp::Reverse, collections::BinaryHeap};
 
 /*
 Q2. Find K Pairs with Smallest Sums
@@ -12,33 +12,17 @@ from the second array.
 Return the k pairs (u1, v1), (u2, v2), ..., (uk, vk) with the smallest sums.
 */
 fn main() {
-    todo!("Problem solved here");
+    let nums1 = vec![1, 7, 11];
+    let nums2 = vec![2, 4, 6];
+    let k = 3;
+    let ans = Solution::k_smallest_pairs(nums1, nums2, k);
+    println!("{:?}", ans);
 }
 
 struct Solution;
 
 impl Solution {
-    pub fn k_smallest_pairs(nums1: Vec<i32>, nums2: Vec<i32>, k: i32) -> Vec<Vec<i32>> {
-        let mut ans: Vec<Vec<i32>> = Vec::new();
-        let mut num1_bh = BinaryHeap::from(nums1.clone());
-        let mut num2_bh = BinaryHeap::from(nums1.clone());
-        let limit: usize;
-
-        if nums1.len() <= nums2.len() {
-            limit = nums1.len() - k as usize;
-        } else {
-            limit = nums2.len() - k as usize;
-        }
-
-        for _ in 0..limit {
-            num1_bh.pop();
-            num2_bh.pop();
-        }
-        for _ in 0..limit {
-            ans.push(vec![num1_bh.pop().unwrap(), num2_bh.pop().unwrap()]);
-        }
-        ans
-    }
+    pub fn k_smallest_pairs(nums1: Vec<i32>, nums2: Vec<i32>, k: i32) -> Vec<Vec<i32>> {}
 }
 
 #[cfg(test)]
