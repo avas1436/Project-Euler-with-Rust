0fedf0f8fdf2e7b696d19187045ac82d5fa012f3
avAs zade <abas.zade@outlook.com>
2026-02-05 18:38:01 +0330

    solve problem succesfully.

--- datastructure/src/bin/010.rs
+++ datastructure/src/bin/010.rs
@@ -11,3 +11,57 @@ n 1's, you may perform the following procedure :
 Return true if it is possible to construct the target array from arr, otherwise, return
 false.
  */
+fn main() {
+    let target = vec![9, 3, 5];
+    println!("{}", Solution::is_possible(target));
+}
+
+struct Solution;
+
+impl Solution {
+    pub fn is_possible(target: Vec<i32>) -> bool {
+        let mut heap = std::collections::BinaryHeap::new();
+        let mut sum = 0;
+
+        for &num in &target {
+            heap.push(num);
+            sum += num;
+        }
+
+        while let Some(max) = heap.pop() {
+            if max == 1 {
+                return true;
+            }
+
+            let rest = sum - max;
+            if rest == 1 {
+                return true;
+            }
+
+            let next = max % rest;
+            if next == 0 || next == max {
+                return false;
+            }
+
+            heap.push(next);
+            sum = rest + next;
+        }
+
+        false
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_is_possible() {
+        assert_eq!(Solution::is_possible(vec![9, 3, 5]), true);
+    }
+
+    #[test]
+    fn test_is_possible_2() {
+        assert_eq!(Solution::is_possible(vec![1, 1, 1, 2]), false);
+    }
+}
