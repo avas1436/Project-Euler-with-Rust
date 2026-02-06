30bfd32da59ad5c171469e0fbfa7bb9ad058afce
avAs zade <abas.zade@outlook.com>
2026-02-06 20:01:24 +0330

    feat(StrMatching-1): implemented structure and test.

--- datastructure/src/bin/014.rs
+++ datastructure/src/bin/014.rs
@@ -7,3 +7,38 @@ appending multiple copies of the substring together.
 fn main() {
     todo!("problem solve here");
 }
+
+struct Solution;
+
+impl Solution {
+    pub fn repeated_substring_pattern(s: String) -> bool {}
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_ab() {
+        assert_eq!(
+            Solution::repeated_substring_pattern("abab".to_string()),
+            true
+        );
+    }
+
+    #[test]
+    fn test_ababab() {
+        assert_eq!(
+            Solution::repeated_substring_pattern("aba".to_string()),
+            false
+        );
+    }
+
+    #[test]
+    fn test_abababab() {
+        assert_eq!(
+            Solution::repeated_substring_pattern("abcabcabcabc".to_string()),
+            true
+        );
+    }
+}
