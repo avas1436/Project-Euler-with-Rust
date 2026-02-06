fbc97ff0cd60f5269c1a6cdbcad7ec7787288d87
avAs zade <abas.zade@outlook.com>
2026-02-06 15:16:48 +0330

    chore(String-2): implement structure and tests.

--- datastructure/src/bin/012.rs
+++ datastructure/src/bin/012.rs
@@ -12,3 +12,33 @@ and you should convert all lowercase letters to uppercase.
 
 Return the reformatted license key.
  */
+fn main() {
+    todo!("problem solve here");
+}
+
+struct Solution;
+
+impl Solution {
+    pub fn license_key_formatting(s: String, k: i32) -> String {}
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_license_key_formatting() {
+        assert_eq!(
+            Solution::license_key_formatting("5F3Z-2e-9-w".to_string(), 4),
+            "5F3Z-2E9W".to_string()
+        );
+    }
+
+    #[test]
+    fn test_license_key_formatting_2() {
+        assert_eq!(
+            Solution::license_key_formatting("2-5g-3-J".to_string(), 2),
+            "2-5G-3J".to_string()
+        );
+    }
+}
