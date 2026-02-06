f1c662f9b44ce4e5d87c2d1f1bb614dc2f8552ac
avAs zade <abas.zade@outlook.com>
2026-02-06 20:53:42 +0330

    feat(StrMatching-2): implemented structure and tests.

--- datastructure/src/bin/015.rs
+++ datastructure/src/bin/015.rs
@@ -9,3 +9,32 @@ A shift on s consists of moving the leftmost character of s to the rightmost pos
     For example, if s = "abcde", then it will be "bcdea" after one shift.
 
  */
+
+fn main() {
+    todo!("problem solve here");
+}
+
+struct Solution;
+
+impl Solution {
+    pub fn rotate_string(s: String, goal: String) -> bool {}
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_rotate_string() {
+        let s = "abcde".to_string();
+        let goal = "cdeab".to_string();
+        assert_eq!(Solution::rotate_string(s, goal), true);
+    }
+
+    #[test]
+    fn test_rotate_string2() {
+        let s = "abcde".to_string();
+        let goal = "abced".to_string();
+        assert_eq!(Solution::rotate_string(s, goal), false);
+    }
+}
