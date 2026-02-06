d7f39d134ee91a686287605429a08ab479046298
avAs zade <abas.zade@outlook.com>
2026-02-06 22:28:36 +0330

    feat(StrMatching-3): implement structure and tests.

--- datastructure/src/bin/016.rs
+++ datastructure/src/bin/016.rs
@@ -8,3 +8,50 @@ be a substring of a after repeating it, return -1.
 Notice: string "abc" repeated 0 times is "", repeated 1 time is "abc" and repeated 2
 times is "abcabc".
  */
+
+fn main() {
+    todo!("problem solve here");
+}
+
+struct Solution;
+
+impl Solution {
+    pub fn repeated_string_match(a: String, b: String) -> i32 {}
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn substring() {
+        assert_eq!(
+            Solution::repeated_string_match("abcd".to_string(), "cdabcdab".to_string()),
+            3
+        );
+    }
+
+    #[test]
+    fn repeated_string_match() {
+        assert_eq!(
+            Solution::repeated_string_match("a".to_string(), "aa".to_string()),
+            3
+        );
+    }
+
+    #[test]
+    fn rotate_string() {
+        assert_eq!(
+            Solution::repeated_string_match("abcde".to_string(), "abced".to_string()),
+            1
+        );
+    }
+
+    #[test]
+    fn zero_rotate() {
+        assert_eq!(
+            Solution::repeated_string_match("abcde".to_string(), "".to_string()),
+            0
+        );
+    }
+}
