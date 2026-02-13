43a236a207ace19c4ab61c806e9445607ef2130d
avAs zade <abas.zade@outlook.com>
2026-02-13 16:20:36 +0330

    Add Solution skeleton and tests for House Robber.

---
+++ DynamicProgramming/src/bin/003.rs
@@ -0,0 +1,42 @@
+/*
+198. House Robber
+
+You are a professional robber planning to rob houses along a street. Each house
+has a certain amount of money stashed, the only constraint stopping you from robbing
+each of them is that adjacent houses have security systems connected and it will
+automatically contact the police if two adjacent houses were broken into on the
+same night.
+
+Given an integer array nums representing the amount of money of each house, return
+the maximum amount of money you can rob tonight without alerting the police.
+ */
+
+fn main() {
+    let _ = Solution::rob(vec![1, 2, 3, 1]);
+}
+
+struct Solution;
+
+impl Solution {
+    pub fn rob(nums: Vec<i32>) -> i32 {}
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_rob() {
+        assert_eq!(Solution::rob(vec![1, 2, 3, 1]), 4);
+    }
+
+    #[test]
+    fn test_rob2() {
+        assert_eq!(Solution::rob(vec![2, 7, 9, 3, 1]), 12);
+    }
+
+    #[test]
+    fn test_rob3() {
+        assert_eq!(Solution::rob(vec![2, 1, 1, 2]), 4);
+    }
+}
